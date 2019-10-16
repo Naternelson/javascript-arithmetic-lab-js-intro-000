ab0af15717f98a3233b970cf002fc4a08b4c1c82
Nathan <7nater.nelson@gmail.com>
2019-10-16 02:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ b = Math.floor(Math.random()*1000)
 
 function makeInt(n) {
   parseInt(n, 10);
-  return n
 }
 a = Math.floor(Math.random()*1000)
 b = Math.floor(Math.random()*1000)
 
 function preserveDecimal(n) {
   parseFloat(n);
+
 }
