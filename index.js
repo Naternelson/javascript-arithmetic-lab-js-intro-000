9b6162f85075ffba64a7da67f9907f64f41f1aa4
Nathan <7nater.nelson@gmail.com>
2019-10-16 02:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ b = Math.floor(Math.random()*1000)
 function add(a,b) {
   return a + b;
 }
+a = Math.floor(Math.random()*1000)
+b = Math.floor(Math.random()*1000)
+
+function subtract(a,b) {
+  return a - b;
+}
